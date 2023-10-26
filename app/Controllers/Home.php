8965673caf186876070f7dc32aeb49e58911b001
MiguelAngelCruzRoman <L21TE0131@teziutlan.tecnm.mx>
2023-10-26 11:36:18 -0600

    Tarjetas para vistas

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,14 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        $db = db_connect();
+        $sql ="select * from alumno";
+        $query =$db->query($sql);
+        $data['alumnos'] = $query ->getResult();
+
+        return view('home/head').
+                view('home/menu').
+                view('home/listaAlumnos',$data).
+                view('home/footer');
     }
 }
